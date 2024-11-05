c23b9f56e27154e70f646bbab9fce069ae5c6f04
Nicolas Marchildon <nicolas@marchildon.net>
2024-11-04 21:56:35 -0500

    Support text user input

--- bot.py
+++ bot.py
@@ -1,11 +1,20 @@
+# bot.py
 import sys
 import json
 from tool_manager import ToolManager
 
+def load_user_input(user_input_file):
+    with open(user_input_file, 'r') as f:
+        content = f.read()
+        try:
+            user_input = json.loads(content)
+        except json.JSONDecodeError:
+            user_input = {"root_tool": "default_tool", "inputs": {"text_input": content}}
+    return user_input
+
 def main():
     user_input_file = sys.argv[1]
-    with open(user_input_file, 'r') as f:
-        user_input = json.load(f)
+    user_input = load_user_input(user_input_file)
     root_tool_name = user_input['root_tool']
     inputs = user_input['inputs']
     manager = ToolManager()
