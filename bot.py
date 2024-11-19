310ee70d95fada457c58b4c01e05fb886b33488f
Nicolas Marchildon <nicolas@marchildon.net>
2024-11-18 20:51:03 -0500

    Split registry_tool out of bot, so it can be inspected

--- bot.py
+++ bot.py
@@ -3,70 +3,50 @@ import json
 import logging
 import os
 import signal
-import socket
 import subprocess
 import sys
+import time
 from pathlib import Path
-from time import sleep
-from typing import Dict
+from typing import Optional
 
 import requests
-from flask import Flask, request, jsonify
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 
-self_name = 'tool_registry'
-self_description = "Registry that can start a tool."
-
+# Setup logging
 Path("logs").mkdir(exist_ok=True)
 logging.basicConfig(filename=f'logs/bot.log', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-# Registry of subprocess tools
-processes = {}
+registry_process: Optional[subprocess.Popen[bytes]] = None
 
-# OpenAPI Object for each tool
-openapi_objects: Dict[str, Dict] = {}
-
-app = Flask(self_name)
-
-retry_strategy = Retry(connect=10, backoff_factor=1)
-
-adapter = HTTPAdapter(max_retries=retry_strategy)
-session = requests.Session()
-session.mount("http://", adapter)
-
-
-### Interactive functions
 
 def interactive(port: int):
-    subprocess_server(port)
+    url = f'http://localhost:{port}'
+    retry_strategy = Retry(connect=10, backoff_factor=1)
+    adapter = HTTPAdapter(max_retries=retry_strategy)
+    session = requests.Session()
+    session.mount("http://", adapter)
     while True:
         try:
             user_input = read_user_input()
-            tool = get_tool(user_input['tool'])
-            response = tool['post'](user_input['input'], resource=user_input['resource'])
+            tool_name = user_input["tool"]
+            start_response = session.post(f'{url}/start', json={"name": tool_name})
+            start_response.raise_for_status()
+            tool_schema = start_response.json()
+            tool_url = tool_schema["servers"][0]["url"]
+            tool_resource = user_input["resource"]
+            tool_response = session.post(f'{tool_url}/{tool_resource}', json=user_input['input'])
+            tool_response.raise_for_status()
+            response = tool_response.json()
             print(response['content'])
         except KeyboardInterrupt:
-            app.logger.warning("Keyboard Interrupt")
+            logger.warning("Keyboard Interrupt")
             shutdown()
         except Exception as e:
-            app.logger.exception("Exception in interactive mode")
+            logger.exception("Exception in interactive mode")
             shutdown()
             return
-
-
-def subprocess_server(port: int):
-    app.logger.info(f"Starting server process on port {port}")
-    with open(f"logs/bot-server.log", "w") as log_file:
-        process = subprocess.Popen(
-            ['python', __file__, '--server', '-p', str(port)],
-            stdout=log_file,
-            stderr=subprocess.STDOUT,
-            start_new_session=True  # Detach process from the parent
-        )
-    app.logger.info(f"Started server: pid {process.pid}, port {port}")
-    # Interactive mode: this is the only tool we'll register
-    return register_tool_process(port, process, self_name)
 
 
 def read_user_input():
@@ -78,156 +58,26 @@ def read_user_input():
     return user_input
 
 
-def get_tool(tool_name):
-    if tool_name in processes.keys():
-        return processes[tool_name]
-    return delegate_start_tool(tool_name)
+def subprocess_server(port: int):
+    logger.info(f"Starting server process on port {port}")
+    with open(f"logs/bot-server.log", "w") as log_file:
+        process = subprocess.Popen(
+            ['python', 'main.py', str(port)],
+            cwd='tools/registry_tool',
+            stdout=log_file,
+            stderr=subprocess.STDOUT,
+            start_new_session=True  # Detach process from the parent
+        )
+    time.sleep(1)
+    logger.info(f"Started server: pid {process.pid}, port {port}")
+    return process
 
 
-def delegate_start_tool(tool_name):
-    """Delegate the task of starting a tool to the server."""
-    openapi = processes[self_name]['post']({"name": tool_name}, '/start')
-    return get_tool_handle(openapi["servers"][0]["url"], tool_name)
-
-
-### Server functions
-
-def start_tool(tool_name, port: int = None):
-    port = port if port else find_free_port()
-    app.logger.info(f"Starting '{tool_name}' on port {port}")
-    process = subprocess.Popen(
-        ['python', 'main.py', str(port)],
-        cwd=os.path.join('tools', tool_name),
-        stdin=subprocess.PIPE
-    )
-    servers = [srv for openapi in openapi_objects.values() for srv in openapi["servers"]]
-    process.stdin.write(json.dumps({'servers': servers}).encode('utf-8'))
-    process.stdin.close()
-    sleep(1)
-    return register_tool_process(port, process, tool_name)
-
-
-def register_tool_process(port, process, tool_name):
-    url = f'http://localhost:{port}'
-    app.logger.info(f"Registering '{tool_name}' at {url}")
-    tool = get_tool_handle(url, tool_name)
-    tool.update({'process': process})
-    processes[tool_name] = tool
-    openapi_objects[tool_name] = tool['get']('/openapi.json')
-    return openapi_objects[tool_name]
-
-
-def get_tool_handle(url, tool_name):
-    def post(data, resource='/'):
-        endpoint = f'{url}{resource}'
-        app.logger.info(f"{tool_name} POST {endpoint} \n{data}")
-        return session.post(endpoint, json=data).json()
-
-    def get(resource='/'):
-        endpoint = f'{url}{resource}'
-        app.logger.info(f"{tool_name} GET {endpoint}")
-        return session.get(endpoint).json()
-
-    return {'get': get, 'post': post}
-
-
-def find_free_port():
-    s = socket.socket()
-    s.bind(('', 0))
-    port = s.getsockname()[1]
-    s.close()
-    return port
-
-
-@app.route('/openapi.json', methods=['GET'])
-def identify():
-    """Self-report tool identification."""
-    host = request.host
-    port = host.split(':')[1] if ':' in host else '80'
-    return jsonify(self_identification(host, port))
-
-
-def self_identification(host, port):
-    response = {
-        "openapi": "3.1.0",
-        "info": {
-            "title": "tool_registry",
-            "description": self_description,
-            "version": "0.0.1",
-            "port": port,
-            "url": f"http://{host}:{port}"
-        },
-        "servers": [{"url": f"http://127.0.0.1:{args.port}", "description": self_description, "x-tool": self_name}],
-        "paths": {
-            "/start": {
-                "post": {
-                    "summary": "Start a tool, adding it to the registry.",
-                    "operationId": "start_tool",
-                    "requestBody": {
-                        "application/json": {
-                            "schema": {
-                                "type": "object",
-                                "required": [
-                                    "name"
-                                ],
-                                "properties": {
-                                    "name": {
-                                        "type": "string",
-                                        "description": "The name of the tool to start, which corresponds to the name of the directory containing a main.py file."
-                                    }
-                                }
-                            }
-                        }
-                    },
-                    "responses": {
-                        "200": {
-                            "description": "Tool has been started.",
-                            "content": {
-                                "application/json": {
-                                    "schema": {
-                                        "$ref": "https://json-schema.org/draft/2020-12/schema"
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            },
-            "/list": {
-                "get": {
-                    "operationId": "list_tools",
-                    "summary": "List the currently running tools.",
-                    "description": "Once a tool has been started, it should be listed in the response."
-                }
-            }
-        }
-    }
-    return response
-
-
-@app.route('/start', methods=['POST'])
-def start_tool_route():
-    """Endpoint to start a tool via HTTP POST request, returning its OpenAPI schema."""
-    tool_name = request.json['name']
-    if tool_name not in processes.keys():
-        start_tool(tool_name)
-    return openapi_objects[tool_name]
-
-
-@app.route('/list', methods=['GET'])
-def list_tools_route():
-    """List the currently running tools."""
-    return jsonify(openapi_objects)
-
-
-@app.route('/shutdown', methods=['POST'])
 def shutdown():
-    """Endpoint to shut down the server and terminate all tools this server has started."""
-    for tool_info in processes.values():
-        tool_process = tool_info.get('process')
-        if tool_process:
-            os.kill(tool_process.pid, signal.SIGTERM)
-            tool_process.wait()
+    """Endpoint to shut down the bot and terminate all tools started."""
+    if registry_process:
+        os.kill(registry_process.pid, signal.SIGTERM)
+        registry_process.wait()
     logging.info("Shutdown complete")
     exit(0)
 
@@ -239,9 +89,8 @@ if __name__ == '__main__':
     args = parser.parse_args(sys.argv[1:])
     signal.signal(signal.SIGTERM, lambda signum, frame: shutdown())
     signal.signal(signal.SIGINT, lambda signum, frame: shutdown())
-
+    registry_process = subprocess_server(args.port)
     if args.server:
-        openapi_objects[self_name] = self_identification('localhost', args.port)
-        app.run(port=args.port)
+        registry_process.wait()
     else:
         interactive(port=args.port)
---
+++ tools/registry_tool/main.py
@@ -0,0 +1,181 @@
+import json
+import logging
+import os
+import signal
+import socket
+import subprocess
+import sys
+from pathlib import Path
+from time import sleep
+from typing import Dict
+
+import requests
+from flask import Flask, request, jsonify
+from requests.adapters import HTTPAdapter
+from urllib3.util.retry import Retry
+
+self_name = 'registry_tool'
+self_description = "Registry that can start tools."
+
+Path("logs").mkdir(exist_ok=True)
+logging.basicConfig(filename=f'registry_tool.log', level=logging.INFO)
+
+# Registry of subprocess tools
+processes = {}
+
+# OpenAPI Object for each tool
+openapi_objects: Dict[str, Dict] = {}
+
+app = Flask(self_name)
+port = int(sys.argv[1])
+
+retry_strategy = Retry(connect=10, backoff_factor=1)
+
+adapter = HTTPAdapter(max_retries=retry_strategy)
+session = requests.Session()
+session.mount("http://", adapter)
+
+
+def start_tool(tool_name):
+    tool_port = find_free_port()
+    app.logger.info(f"Starting '{tool_name}' on port {tool_port}")
+    process = subprocess.Popen(
+        ['python', 'main.py', str(tool_port)],
+        cwd=os.path.join('..', tool_name),
+        stdin=subprocess.PIPE
+    )
+    servers = [srv for openapi in openapi_objects.values() for srv in openapi["servers"]]
+    process.stdin.write(json.dumps({'servers': servers}).encode('utf-8'))
+    process.stdin.close()
+    sleep(1)
+    return register_tool_process(tool_port, process, tool_name)
+
+
+def register_tool_process(tool_port, process, tool_name):
+    url = f'http://localhost:{tool_port}'
+    app.logger.info(f"Registering '{tool_name}' at {url}")
+    tool = get_tool_handle(url, tool_name)
+    tool.update({'process': process})
+    processes[tool_name] = tool
+    openapi_objects[tool_name] = tool['get']('/openapi.json')
+    return openapi_objects[tool_name]
+
+
+def get_tool_handle(url, tool_name):
+    def post(data, resource='/'):
+        endpoint = f'{url}{resource}'
+        app.logger.info(f"{tool_name} POST {endpoint} \n{data}")
+        return session.post(endpoint, json=data).json()
+
+    def get(resource='/'):
+        endpoint = f'{url}{resource}'
+        app.logger.info(f"{tool_name} GET {endpoint}")
+        return session.get(endpoint).json()
+
+    return {'get': get, 'post': post}
+
+
+def find_free_port():
+    s = socket.socket()
+    s.bind(('', 0))
+    free_port = s.getsockname()[1]
+    s.close()
+    return free_port
+
+
+@app.route('/openapi.json', methods=['GET'])
+def schema_route():
+    """Self-report tool identification."""
+    return jsonify(self_schema(request.host))
+
+
+def self_schema(host):
+    response = {
+        "openapi": "3.1.0",
+        "info": {
+            "title": "registry_tool",
+            "description": self_description,
+            "version": "0.0.1",
+            "port": port,
+            "url": f"http://{host}:{port}"
+        },
+        "servers": [{"url": f"http://127.0.0.1:{port}", "description": self_description, "x-tool": self_name}],
+        "paths": {
+            "/start": {
+                "post": {
+                    "summary": "Start a tool, adding it to the registry.",
+                    "operationId": "start_tool",
+                    "requestBody": {
+                        "application/json": {
+                            "schema": {
+                                "type": "object",
+                                "required": [
+                                    "name"
+                                ],
+                                "properties": {
+                                    "name": {
+                                        "type": "string",
+                                        "description": "The name of the tool to start, which corresponds to the name of the directory containing a main.py file."
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    "responses": {
+                        "200": {
+                            "description": "Tool has been started.",
+                            "content": {
+                                "application/json": {
+                                    "schema": {
+                                        "$ref": "https://json-schema.org/draft/2020-12/schema"
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            "/list": {
+                "get": {
+                    "operationId": "list_tools",
+                    "summary": "List the currently running tools.",
+                    "description": "Once a tool has been started, it should be listed in the response."
+                }
+            }
+        }
+    }
+    return response
+
+
+@app.route('/start', methods=['POST'])
+def start_tool_route():
+    """Endpoint to start a tool via HTTP POST request, returning its OpenAPI schema."""
+    tool_name = request.json['name']
+    if tool_name not in processes.keys():
+        start_tool(tool_name)
+    return openapi_objects[tool_name]
+
+
+@app.route('/list', methods=['GET'])
+def list_tools_route():
+    """List the currently running tools."""
+    return jsonify(openapi_objects)
+
+
+@app.route('/shutdown', methods=['POST'])
+def shutdown():
+    """Endpoint to shut down the server and terminate all tools this server has started."""
+    for tool_info in processes.values():
+        tool_process = tool_info.get('process')
+        if tool_process:
+            os.kill(tool_process.pid, signal.SIGTERM)
+            tool_process.wait()
+    logging.info("Shutdown complete")
+    exit(0)
+
+
+if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, lambda signum, frame: shutdown())
+    signal.signal(signal.SIGINT, lambda signum, frame: shutdown())
+    openapi_objects[self_name] = self_schema('localhost')
+    app.run(port=port)
